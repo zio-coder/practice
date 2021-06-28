5cade80958654f8d857351724693fec5e6f8453d
zio-coder <79557687+zio-coder@users.noreply.github.com>
2021-06-28 17:25:23 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,15 +1,11 @@
-if (input.temperature() > 30) {
-    basic.showIcon(IconNames.StickFigure)
-    music.startMelody(music.builtInMelody(Melodies.Baddy), MelodyOptions.Once)
-} else {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        `)
-}
+input.onButtonPressed(Button.A, function () {
+    걸음수 = 0
+})
+input.onGesture(Gesture.LogoUp, function () {
+    걸음수 += 1
+})
+let 걸음수 = 0
+걸음수 = 0
 basic.forever(function () {
-	
+    basic.showNumber(걸음수)
 })
